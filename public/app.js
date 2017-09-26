1a5deb201b7c87acbff376e3ba3f3ddcf3048fcb
bbaxa16 <brooke769@gmail.com>
2017-09-26 00:49:23 -0500

    check login func is a go

--- public/app.js
+++ public/app.js
@@ -6,23 +6,33 @@ app.controller('Controller', ['$http', function($http){
   this.message = 'poop';
   this.url = 'http://localhost:3000/';
   this.login = function(userPass){
-  $http({
-    method: 'POST',
-    url: this.url + '/users/login',
-    data: { user: { username: userPass.username, password: userPass.password }},
-  }).then(function(response){
-    if(response.data.message === 'Unauthorized'){
-      console.log(response);
-      this.error = 'username or password was incorrect'
-      this.unauthorized = true;
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: { user: { username: userPass.username, password: userPass.password }},
+    }).then(function(response){
+      if(response.data.message === 'Unauthorized'){
+        console.log(response);
+        this.error = 'username or password was incorrect'
+        this.unauthorized = true;
+      }
+      else {
+        this.user = response.data.user;
+        localStorage.setItem('token', JSON.stringify(response.data.token));
+        localStorage.setItem('logged', JSON.stringify(true));
+        console.log('successful login');
+        this.checkLogin();
+      }
+    }.bind(this));
+  }
+  this.checkLogin = function() {
+    if (localStorage.logged === "true"){
+      controller.logged = true;
+      console.log('we logged in foo');
+    } else {
+      controller.logged = false;
+      console.log('we not');
     }
-    else {
-      this.user = response.data.user;
-      localStorage.setItem('token', JSON.stringify(response.data.token));
-      localStorage.setItem('logged', JSON.stringify(true));
-      console.log('successful login');
-      //this.checkLogin();
-    }
-  }.bind(this));
-}
+  }
+  this.checkLogin();
 }]);
